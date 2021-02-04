b7326e0e4de94b74f929279f7f3b815a8f76436c
Omar Shah <oemyoem55@gmail.com>
2021-02-04 15:55:02 +0500

    Added Tailwind Support to Styled Components

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,20 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+import tw from "tailwind.macro"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const Header = () => (
+  <HeaderContainer>
+    <H1>Ali</H1>
+    <H1>Hussain</H1>
+  </HeaderContainer>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
 export default Header
+
+export const HeaderContainer = styled.header`
+  ${tw`flex justify-between p-4 lg:px-10 lg:py-5 bg-blue-500 text-blue-100 md:sticky top-0 left-0`}
+`
+
+export const H1 = styled.h1`
+  ${tw`text-xl hover:text-blue-200`}
+`
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  container: {
+    center: true,
+    padding: "1rem",
+  },
+}
